2be4f0472434049d50054cee99fb3c8677e9de04
Carlos F. R. Pires <cfrpires@gmail.com>
2022-10-04 14:11:27 -0300

    fiz o menu

--- main.c
+++ main.c
@@ -2,36 +2,11 @@
 
 #define MAX 10
 
-int main() {
     float lista[MAX];
     int pos, i, elementos, quant = 0, valor;
-  
-        printf(" \nDigite a quantidade de elementos:  ");
-        scanf(" %d", &elementos);
-        printf(" \nDigite os valor para os elementos da lista: \n ");
-    
-        for (i = 0; i < elementos; i++) {
-            printf("Posicao[%d] = ", i+1);
-            scanf(" %f", &lista[i]);
-            if (lista[i] != 0)
-            quant++;
-        }
-        printf("Imprime lista:\n");
-        for (i = 0; i <= elementos - 1; i++) {
-            printf(" lista[%d] = ", i+1);
-            printf(" %.f \n", lista[i]);
-        }
-    
-        printf("Digite a posicao do elemento que deseja visualizar o valor:");
-        scanf("%d", &pos);
-    
-        for (i = 0; i < elementos-1; i++) {
-            if (lista[i] == pos) {
-                printf("Posicao %d = %.f\n", pos, lista[i]);
-            }
-        }
-    
-        printf("Digite a posicao do elemento que deseja inserir um novo valor:");
+
+ void acrescenta (){
+    printf("Digite a posicao do elemento que deseja inserir um novo valor:");
         scanf("%d", &pos);
         
         printf("Digite o valor:");
@@ -44,8 +19,10 @@ int main() {
             printf(" lista[%d] = ", i+1);
             printf(" %.f \n", lista[i]);
         }
-    
-        printf("Digite a posicao do elemento que deseja deletar: \n ");
+ }
+
+ void deleta (){
+    printf("Digite a posicao do elemento que deseja deletar: \n ");
         scanf(" %d", &pos);
         if (pos > elementos + 1) {
             printf(" \nErro, posicao invalida!\n.");
@@ -59,7 +36,58 @@ int main() {
               
             }
         } 
-        
+ }
+
+ void visualiza (){
+        printf("sua lista esta assim\n");
+        for (i = 0; i <= elementos - 1; i++) {
+            printf(" lista[%d] = ", i+1);
+            printf(" %.f \n", lista[i]);
+        }
         printf("Quantidade de elementos: %d", quant);
-        return 0;
+ }
+
+int main(){
+    
+    char escolhaMenu; 
+
+  
+    printf(" \nDigite a quantidade de elementos:  ");
+    scanf(" %d", &elementos);
+    printf(" \nDigite os valor para os elementos da lista: \n ");
+
+    for (i = 0; i < elementos; i++) {
+        printf("Posicao[%d] = ", i+1);
+        scanf(" %f", &lista[i]);
+        if (lista[i] != 0)
+        quant++;
+    }
+    do{
+        scanf("%c", &escolhaMenu);
+        
+        printf("\n\nOla bem vindo ao painel de intercao da lista, escolha a funcao desejada:\n \t (1) - Acrescenta\n \t (2) - Deleta\n \t (3) - Visualiza\n \t (0) - Sair\n");
+        scanf("%c", &escolhaMenu);
+
+        switch (escolhaMenu)
+        {
+        case '0':
+            escolhaMenu = '0';
+            break;
+        case '1':
+            acrescenta();
+            break;
+        case '2':
+            deleta();
+            break;
+        case '3':
+            visualiza();
+            break;
+        default:
+            printf("Erro valor inexistente.");
+            break;
+        }
+
+    } while (escolhaMenu != '0');
+    
+    return 0;
 }
